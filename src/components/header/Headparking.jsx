37a03cb0719887f2b86e9e23b1f6cd56733de58b
christian-berdeja <uriel882@gmail.com>
2025-04-22 22:18:20 -0600

    solucion de modo oscuro, renderiza nombre en header

--- src/components/header/Headparking.jsx
+++ src/components/header/Headparking.jsx
@@ -1,6 +1,7 @@
 import styles from './header.module.css'
 import car from '/src/assets/img/car.svg'
 import { useState } from 'react'
+import { jwtDecode } from "jwt-decode";
 import Modal from "../modal/Modal.jsx";
 import RegisterPage from "../../pages/RegisterPage.jsx";
 import Login from "../../pages/LoginPage.jsx";
@@ -8,6 +9,8 @@ import Login from "../../pages/LoginPage.jsx";
 function Headparking() {
   const [isModalRegisterOpen, setIsModalRegisterOpen] = useState(false);
   const [isModalLoginOpen, setIsModalLoginOpen] = useState(false);
+  const jwt = localStorage.getItem("jwt");
+    const decode = jwt? jwtDecode(jwt) : null;
 
   const handleModal = (isRegister = false) => {
     setIsModalRegisterOpen(false);
@@ -23,6 +26,14 @@ function Headparking() {
         <div className={styles.left}>
           <img src={car} alt="Logo" className={styles.logo} />
           <h1 className={styles.title}>Parking app</h1>
+        </div>
+        <div>
+            {/* <h2 style={{ textAlign: 'center', fontSize: '1.8rem', color: '#2c3e50', marginBottom: '20px' }}> */}
+            <h2>
+            {
+                decode? <p>Bienvenido {decode.name} { decode.lastName}  </p> : ""
+            }
+            </h2>
         </div>
         <div className={styles.right}>
           <button className={styles.btn} onClick={() => setIsModalLoginOpen(true)}>Login</button>
