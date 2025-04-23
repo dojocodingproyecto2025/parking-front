90940458087d988fa3384e41e06e1d49d3803fb7
Julidvlp <valderrama687@gmail.com>
2025-04-23 02:25:53 -0500

    ajustes de slide horizontal

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -5,7 +5,10 @@ const Hero = () => {
   return (
     <div className={styles['hero-container']}>
       <div className={styles['hero-content']}>
-        <h1>Reserva tu Estacionamiento</h1>
+        <h1>
+          Parking App
+          <br />
+          Reserva tu Estacionamiento</h1>
         <p>Encuentra y reserva el mejor lugar para tu vehículo</p>
         <button className={styles['cta-button']}>Reservar Ahora</button>
       </div>
